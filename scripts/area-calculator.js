a8a84c3682a49bb68508bea31f39309fcdac9ca9
rafiul33 <rafiulislam2629@gmail.com>
2024-02-10 22:09:16 -0500

    done

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,63 +1,36 @@
-function calculateTriangleArea(id){
-  // get base value
-  const baseField = document.getElementById(id);
-  const baseValueText = baseField.value;
-  const base = parseFloat(baseValueText);
-
-  // get height value 
-  const heightField = document.getElementById('triangle-height');
-  const heightValueText = heightField.value;
-  const height = parseFloat(heightValueText);
-  // Area calculation
-  const area = 0.5 * base * height;
-
-  // show triangle area
-  const areaSpan = document.getElementById('triangle-area');
-  areaSpan.innerText = area;
-
+function calculateTriangleArea(){
+  const base = getInputValue('triangle-base');
+  const length = getInputValue('triangle-height');
+  const area = validateValue(base, length);
+  setElementInnerText('triangle-area', area);
 };
 
 // Rectangle
 function calculateRectangleArea(){
-  // get width
-  const widthField = document.getElementById('rectangle-width');
-  const widthFieldText = widthField.value;
-  const width = parseFloat(widthFieldText);
-  console.log(width);
-
-  // get length
-  const lengthField = document.getElementById('rectangle-length');
-  const lengthFieldText = lengthField.value;
-  const length = parseFloat(lengthFieldText);
-  console.log(length);
-
-  // calculate area
-  const area = width * length;
-  // set rectangle area
-  const areaSpan = document.getElementById('rectangle-area');
-  areaSpan.innerText = area;
+  const width = getInputValue('rectangle-width');
+  const length = getInputValue('rectangle-length');
+  const area = validateValue(width, length);
+  setElementInnerText('rectangle-area', area);
 };
 
 // Parallelogram
 function calculateParallelogramArea(){
   const base = getInputValue('parallelogram-base');
   const height = getInputValue('parallelogram-height');
-
-  const area = base * height;
-  // const areaSpan = document.getElementById('parallelogram-area');
-  // areaSpan.innerText = area;
+  const area = validateValue(base, height);
   setElementInnerText('parallelogram-area', area);
+  addToCalculationEntry('parallelogram', area);
 };
 
 // ellipse
 function calculateEllipseArea(){
   const majorRadius = getInputValue('ellipse-major-radius');
   const minorRadius = getInputValue('ellipse-minor-radius');
-
-  const area = 3.14 * majorRadius * minorRadius;
+  const area = validateValue(majorRadius, minorRadius);
   const areaTwoDecimal = area.toFixed(2);
   setElementInnerText('ellipse-area', areaTwoDecimal);
-}
+};
+
 // function: get input value field in number
 function getInputValue(fieldId){
   const inputField = document.getElementById(fieldId);
@@ -71,5 +44,37 @@ function getInputValue(fieldId){
 function setElementInnerText(elementId, area) {
   const element = document.getElementById(elementId);
   element.innerText = area;
-  
-}
+};
+// add to calculation entry
+/**
+ * 1. get the element where you want to add the dynamic html
+ * 2. create an element you want to add.
+ * 3. if needed add some class
+ * 4. set inner HTML. it could be dynamic template string
+ * 5. append the created element as child
+ */
+function addToCalculationEntry(areaType, area){
+  console.log(areaType+ ' '+area);
+  const calculationEntry = document.getElementById('calculation-entry');
+  const count = calculationEntry.childElementCount;
+
+  const p = document.createElement('p');
+  p.classList.add('my-4');
+  p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-primary">Convert to m<sup>2</sup></button>`;
+  calculationEntry.appendChild(p)
+};
+
+
+
+// validation:
+function validateValue(value1, value2){
+  let area = 0;
+  if(isNaN(value1)||isNaN(value2)){
+    alert('provide number');
+    return area;
+  }
+  else {
+     area = value1 * value2;
+  }
+  return area;
+};
